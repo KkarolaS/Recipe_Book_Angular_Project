1130b592420d53362037185043695afdb0aff371
KkarolaS <133031923+KkarolaS@users.noreply.github.com>
2023-12-21 06:28:36 +0100

    add functionality add ingredient to shopping list

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Output,
+  ViewChild,
+} from '@angular/core';
+import { Ingredient } from '../../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
-  styleUrl: './shopping-edit.component.css'
+  styleUrl: './shopping-edit.component.css',
 })
 export class ShoppingEditComponent {
+  @ViewChild('nameInput', { static: true }) nameInput: ElementRef;
+  @ViewChild('amountInput', { static: true }) amountInput: ElementRef;
 
+  @Output() ingredientCreated = new EventEmitter<Ingredient>();
+
+  newIngredientName = '';
+  newIngredientAmount = 0;
+
+  newIngredient = new Ingredient(
+    this.newIngredientName,
+    this.newIngredientAmount
+  );
+
+  onShoppingListAdd() {
+    this.newIngredient.name = this.nameInput.nativeElement.value;
+    this.newIngredient.amount = this.amountInput.nativeElement.value;
+
+    this.ingredientCreated.emit(this.newIngredient);
+  }
 }
