1e160d262cc00611054e3a8f9dc9f3455e736901
KkarolaS <133031923+KkarolaS@users.noreply.github.com>
2023-12-21 06:40:12 +0100

    make the code of functionality add ingredient to shopping list better

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -13,23 +13,17 @@ import { Ingredient } from '../../shared/ingredient.model';
   styleUrl: './shopping-edit.component.css',
 })
 export class ShoppingEditComponent {
-  @ViewChild('nameInput', { static: true }) nameInput: ElementRef;
-  @ViewChild('amountInput', { static: true }) amountInput: ElementRef;
+  @ViewChild('nameInput', { static: true }) nameInputRef: ElementRef;
+  @ViewChild('amountInput', { static: true }) amountInputRef: ElementRef;
 
   @Output() ingredientCreated = new EventEmitter<Ingredient>();
 
-  newIngredientName = '';
-  newIngredientAmount = 0;
-
-  newIngredient = new Ingredient(
-    this.newIngredientName,
-    this.newIngredientAmount
-  );
-
   onShoppingListAdd() {
-    this.newIngredient.name = this.nameInput.nativeElement.value;
-    this.newIngredient.amount = this.amountInput.nativeElement.value;
+    const newIngName = this.nameInputRef.nativeElement.value;
+    const newIngAmount = this.amountInputRef.nativeElement.value;
 
-    this.ingredientCreated.emit(this.newIngredient);
+    const newIngredient = new Ingredient(newIngName, newIngAmount);
+
+    this.ingredientCreated.emit(newIngredient);
   }
 }
